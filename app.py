0995c2d34bc04ee516c8a439dddb9733f350520a
PERZ1AN <gibetinho2000@hotmail.com>
2021-12-14 02:19:31 +0200

    for first presentation

---
+++ app.py
@@ -0,0 +1,45 @@
+import MySQLdb
+from flask import Flask, render_template, request, redirect, session
+from flask.helpers import url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = "secretest_secret"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'wt'
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/home')
+def homepage():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE email = %s', (session['email'],))
+        account = cursor.fetchone()
+        return render_template('homepage.html', account = account)
+    return render_template('homepage.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def loginpage():
+    if request.method == 'GET':
+        if 'loggedin' in session:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM accounts WHERE email = %s', (session['email'],))
+            account = cursor.fetchone()
+            return render_template('loginpage.html', account = account)
+        return render_template('loginpage.html')
+    if request.method == 'POST' and 'email' in request.form and 'psw' in request.form:
+        email = request.form['email']
+        psw = request.form['psw']
+        print("Username: " + email + " Password: " + psw)
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("select * from accounts where email = %s and password = %s", (email, psw))
+        account = cur.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['email'] = account['email']
+            session['username'] = account['username']
+            return redirect(url_for('homepage', account = account))
+        return redirect(url_for('loginpage'))
