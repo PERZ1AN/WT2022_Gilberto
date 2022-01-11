988bf2cef3fd7078b07e6a7427e045cbc4f1db9f
PERZ1AN <gibetinho2000@hotmail.com>
2022-01-11 08:59:15 +0200

    part 4 presentation

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-"""
-For rental system:
--check against db date
--
-"""
-
-
-
-
-
-
+from re import escape
 import MySQLdb
 from flask import Flask, render_template, request, redirect, session
 from flask.helpers import url_for
 from flask_mysqldb import MySQL
+import datetime
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = "secretest_secret"
@@ -39,7 +30,7 @@ def homepage():
         cursor.execute('SELECT * FROM accounts WHERE email = %s', (session['email'],))
         account = cursor.fetchone()
         return render_template('homepage.html', account = account, imggrid = ret_movies())
-    return render_template('homepage.html')
+    return render_template('homepage.html', imggrid = ret_movies())
 
 @app.route('/login', methods=['GET', 'POST'])
 def loginpage():
@@ -70,8 +61,15 @@ def profile():
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE email = %s', (session['email'],))
         account = cursor.fetchone()
-        return render_template('profile.html', account = account)
-    return redirect(url_for('login'))
+        cursor.execute('select mid, due from rentals where cemail = %s', (session['email'],))
+        rentals = cursor.fetchall()
+        actives = []
+        for rental in rentals:
+            cursor.execute('select name from movies where id = %s', (rental['mid'],))
+            mname = cursor.fetchone()
+            actives.append([mname['name'], rental['due']])
+        return render_template('profile.html', account = account, actives = actives)
+    return redirect(url_for('loginpage'))
 
 @app.route('/logout', methods = ['GET'])
 def logout():
@@ -83,7 +81,25 @@ def logout():
 @app.route('/movies/<id>')
 def movie(id):
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM movies WHERE {}'.format(id))
+    cursor.execute('SELECT * FROM movies WHERE id={}'.format(id))
     movie = cursor.fetchone()
     print(movie)
     return render_template('moviepage.html', movie = movie)
+
+@app.route('/movies/rent/<id>', methods = ['POST'])
+def rent(id):
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select * from rentals where mid={}'.format(id))
+        check = cursor.fetchone()
+        if check:
+            return "<h1> The movie you selected is already available to you. <a href='/home'> Return to homepage. </a></h1>"
+        cursor.execute('SELECT * FROM movies WHERE id={}'.format(id))
+        movie = cursor.fetchone()
+        now = datetime.datetime.now()
+        due = now + datetime.timedelta(days=2)
+        cursor.execute("insert into rentals (cemail, mid, due) values (%s, %s, %s)", (session['email'], movie['id'], due.strftime('%Y-%m-%d %H:%M:%S')))
+        mysql.connection.commit()
+        return "<h1> The movie '{}' was rented at {}. <a href='/home'> Return to homepage. </a></h1>".format(movie['name'], now)
+    return redirect(url_for('loginpage'))
+
